3ed0b3c3c4077ad4fe996f2199f201831153ba9d
codegirl77 <rachelklinkatsis@gmail.com>
2019-10-21 13:43:49 -0700

    added cors to server

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
  
 app.use(bodyParser.json())
